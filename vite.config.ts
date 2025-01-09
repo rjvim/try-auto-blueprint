642c4b9da5dc684bfc4f0431ad75b0331fb3f9fd
Rajiv Seelam <rajiv@betalectic.com>
2025-01-09 13:40:00 +0530

    refactor: update package name and add glob dependency; adjust Vite config for dynamic entry points

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 // import { peerDependencies } from "./package.json";
 import { defineConfig } from "vite";
+import pkg from "glob";
+const { glob } = pkg;
+import { fileURLToPath } from "node:url";
+import { extname, relative } from "path";
 
 export default defineConfig({
   plugins: [
@@ -14,13 +18,35 @@ export default defineConfig({
   build: {
     lib: {
       entry: "./src/index.ts",
-      name: "blueprint",
-      fileName: (format) => `bp.${format}.js`,
-      formats: ["es", "cjs", "umd"],
+      // name: "blueprint",
+      // fileName: (format) => `bp.${format}.js`,
+      // formats: ["es", "cjs", "umd"],
+      formats: ["es"],
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime", "react-dom"],
-      output: { globals: { react: "React", "react-dom": "ReactDOM" } },
+
+      input: Object.fromEntries(
+        // https://rollupjs.org/configuration-options/#input
+        glob
+          .sync("src/**/*.{ts,tsx}", {
+            ignore: ["src/**/*.d.ts"],
+          })
+          .map((file) => [
+            // 1. The name of the entry point
+            // lib/nested/foo.js becomes nested/foo
+            relative("src", file.slice(0, file.length - extname(file).length)),
+            // 2. The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+
+      output: {
+        globals: { react: "React", "react-dom": "ReactDOM" },
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
     },
   },
 });
