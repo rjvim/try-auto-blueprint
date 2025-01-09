c0712362f3b2368ef6b26fa0226845a2c350cc8d
Rajiv Seelam <rajiv@betalectic.com>
2025-01-09 14:55:58 +0530

    fix: adjust className order in label component for proper styling

--- src/components/label/label.tsx
+++ src/components/label/label.tsx
@@ -3,5 +3,5 @@ import styles from "./styles.module.css";
 export function Label(props: React.LabelHTMLAttributes<HTMLLabelElement>) {
   const { className, ...restProps } = props;
   console.log("Loading label");
-  return <label className={`${className}  ${styles.label}`} {...restProps} />;
+  return <label className={`${styles.label} ${className}`} {...restProps} />;
 }
