e759b3aba4f0028d032443f387d01b812ac6847b
Rajiv Seelam <rajiv@betalectic.com>
2025-01-09 12:34:12 +0530

    feat: add Button and FormField exports to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { Card } from "./components/card";
 export { Label } from "./components/label";
 export { Input } from "./components/input";
+export { Button } from "./components/button";
+export { FormField } from "./components/form-field";
