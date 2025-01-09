e3f44ab8539e0e2f828ea340818bb14a3cb063d2
Rajiv Seelam <rajiv@betalectic.com>
2025-01-09 14:40:29 +0530

    fix: update package.json exports and restore Vite config options

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 // import { peerDependencies } from "./package.json";
 import { defineConfig } from "vite";
-import pkg from "glob";
-const { glob } = pkg;
-import { fileURLToPath } from "node:url";
-import { extname, relative } from "path";
+// import pkg from "glob";
+// const { glob } = pkg;
+// import { fileURLToPath } from "node:url";
+// import { extname, relative } from "path";
 
 export default defineConfig({
   plugins: [
@@ -18,34 +18,34 @@ export default defineConfig({
   build: {
     lib: {
       entry: "./src/index.ts",
-      // name: "blueprint",
-      // fileName: (format) => `bp.${format}.js`,
-      // formats: ["es", "cjs", "umd"],
-      formats: ["es"],
+      name: "blueprint",
+      fileName: (format) => `bp.${format}.js`,
+      formats: ["es", "cjs", "umd"],
+      // formats: ["es"],
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime", "react-dom"],
 
-      input: Object.fromEntries(
-        // https://rollupjs.org/configuration-options/#input
-        glob
-          .sync("src/**/*.{ts,tsx}", {
-            ignore: ["src/**/*.d.ts"],
-          })
-          .map((file) => [
-            // 1. The name of the entry point
-            // lib/nested/foo.js becomes nested/foo
-            relative("src", file.slice(0, file.length - extname(file).length)),
-            // 2. The absolute path to the entry file
-            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url)),
-          ])
-      ),
+      // input: Object.fromEntries(
+      //   // https://rollupjs.org/configuration-options/#input
+      //   glob
+      //     .sync("src/**/*.{ts,tsx}", {
+      //       ignore: ["src/**/*.d.ts"],
+      //     })
+      //     .map((file) => [
+      //       // 1. The name of the entry point
+      //       // lib/nested/foo.js becomes nested/foo
+      //       relative("src", file.slice(0, file.length - extname(file).length)),
+      //       // 2. The absolute path to the entry file
+      //       // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+      //       fileURLToPath(new URL(file, import.meta.url)),
+      //     ])
+      // ),
 
       output: {
         globals: { react: "React", "react-dom": "ReactDOM" },
-        assetFileNames: "assets/[name][extname]",
-        entryFileNames: "[name].js",
+        // assetFileNames: "assets/[name][extname]",
+        // entryFileNames: "[name].js",
       },
     },
   },
